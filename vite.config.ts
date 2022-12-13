ebc68e80cf72db331c84341615ff2f8c681d3e80
Brandon Reid <brandonreidak@gmail.com>
2022-12-13 13:03:36 -0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
+export default defineConfig(({ mode }) => {
+  const baseConfig = {
+    resolve: {
+      alias: [
+        {
+          find: "@/demo",
+          replacement: resolve(__dirname, "./demo"),
+        },
+        {
+          find: "@",
+          replacement: resolve(__dirname, "src"),
+        },
+      ],
+    },
+    plugins: [vue()],
+  };
+
+  if (mode == "demo") {
+    return {
+      root: "./demo",
+      ...baseConfig,
+    };
+  }
+
+  return {
+    ...baseConfig,
+    build: {
+      emptyOutDir: false,
+      sourcemap: true,
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "orion-design",
+      },
+      rollupOptions: {
+        // ensures vue isn't added to the bundle
+        external: ["vue", "vue-router", "vee-validate"],
+        output: {
+          exports: "named",
+          // Provide vue as a global variable to use in the UMD build
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
+    },
+  };
+});
