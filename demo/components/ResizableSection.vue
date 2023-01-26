37ec4aa2bc45bbbc038ead19d9423cc0af371fd4
Brandon Reid <brandonreidak@gmail.com>
2023-01-26 13:23:00 -0500

    fix demo

--- demo/components/ResizableSection.vue
+++ demo/components/ResizableSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div ref="container" class="resizable-section">
     <!-- <p-frame :style="styles.iframe" :class="classes.iframe"> -->
-    <p-frame body-class="resizable-section__body" :style="styles.iframe" :class="classes.iframe">
+    <p-frame :body-class="classes.iframe" :style="styles.iframe">
       <div class="resizable-section__content">
         <slot />
       </div>
