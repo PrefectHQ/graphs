e6fd79c5816d39c39f789a4a2e43300aa19f8785
Brandon Reid <brandonreidak@gmail.com>
2023-08-23 12:08:55 -0300

    update guide heights on viewport change

--- src/utilities/viewport.ts
+++ src/utilities/viewport.ts
@@ -3,17 +3,18 @@ import { Viewport } from 'pixi-viewport'
 export type ViewportUpdatedCheck = () => boolean
 
 export function viewportUpdatedFactory(viewport: Viewport): ViewportUpdatedCheck {
-  let { left: previousLeft, right: previousRight } = viewport
+  let { left: previousLeft, right: previousRight, height: previousHeight } = viewport
 
   function updated(): boolean {
-    const { left, right } = viewport
+    const { left, right, height } = viewport
 
-    if (left === previousLeft && right === previousRight) {
+    if (left === previousLeft && right === previousRight && height === previousHeight) {
       return false
     }
 
     previousLeft = left
     previousRight = right
+    previousHeight = height
 
     return true
   }
