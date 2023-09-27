0bbd2aebf66619813581ec69242440bda931f268
Craig Harshbarger <pleek91@gmail.com>
2023-09-27 15:25:40 -0500

    Raneme "FlowRunGraph" to "RunGraph"

---
+++ src/components/RunGraph.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="run-graph">
+    hello world
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { RunGraphConfig } from '@/models/RunGraph'
+
+  defineProps<{
+    config: RunGraphConfig,
+  }>()
+</script>
