5ff98457300d78ac58e8e87e1d7839a1e15ed8ec
Craig Harshbarger <pleek91@gmail.com>
2023-10-04 15:50:26 -0500

    Render nodes!

--- src/objects/nodes.ts
+++ src/objects/nodes.ts
@@ -1,6 +1,7 @@
 import { UseSubscription, useSubscription } from '@prefecthq/vue-compositions'
+import { Graphics } from 'pixi.js'
 import { watch } from 'vue'
-import { RunGraphConfig, RunGraphFetch } from '@/models/RunGraph'
+import { RunGraphConfig, RunGraphFetch, RunGraphNode } from '@/models/RunGraph'
 import { waitForConfig } from '@/objects/config'
 import { emitter } from '@/objects/events'
 import { waitForScales } from '@/objects/scales'
@@ -10,10 +11,10 @@ import { effectScopeFactory } from '@/utilities/effectScope'
 let subscription: UseSubscription<RunGraphFetch> | null = null
 
 const scope = effectScopeFactory()
+const nodes = new Map<string, Graphics>()
+let yOffset = 1
 
 export async function startNodes(): Promise<void> {
-  const viewport = await waitForViewport()
-  const { scaleX, scaleY } = await waitForScales()
   const config = await waitForConfig()
 
   startSubscription(config)
@@ -23,6 +24,38 @@ export async function startNodes(): Promise<void> {
 
 export function stopNodes(): void {
   stopSubscription()
+  nodes.clear()
+}
+
+async function renderNode(node: RunGraphNode): Promise<void> {
+  const { scaleX, scaleY } = await waitForScales()
+  const graphics = nodes.get(node.id) ?? await createNode(node.id)
+
+  graphics.clear()
+
+  const offset = yOffset++
+  const x = scaleX(node.start_time)
+  const y = scaleY(offset)
+  const width = scaleX(node.end_time ?? new Date()) - x
+  const height = scaleY(offset + 10) - y
+
+  graphics.beginFill(0x3e494b)
+  graphics.lineStyle(1, 0x0, 1, 2)
+  graphics.drawRoundedRect(x, y, width, height, 8)
+  graphics.endFill()
+
+  graphics.width = Math.max(width, 1)
+}
+
+async function createNode(nodeId: string): Promise<Graphics> {
+  const viewport = await waitForViewport()
+  const graphics = new Graphics()
+
+  nodes.set(nodeId, graphics)
+
+  viewport.addChild(graphics)
+
+  return graphics
 }
 
 function startSubscription({ fetch, runId }: RunGraphConfig): void {
@@ -35,7 +68,7 @@ function startSubscription({ fetch, runId }: RunGraphConfig): void {
     subscription = useSubscription(fetch, [runId])
 
     watch(() => subscription?.response, response => {
-      console.log({ response })
+      response?.nodes.forEach(node => renderNode(node))
     }, { immediate: true })
   })
 }
