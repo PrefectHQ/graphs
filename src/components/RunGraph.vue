e7ef54a5b81eb0b88770a95a800dde0de6adb964
Craig Harshbarger <pleek91@gmail.com>
2023-10-04 12:50:57 -0500

    Remove the domain composition

--- src/components/RunGraph.vue
+++ src/components/RunGraph.vue
@@ -4,10 +4,9 @@
 
 <script lang="ts" setup>
   import { onBeforeUnmount, onMounted, ref } from 'vue'
-  import { useRunGraphDomain } from '@/compositions/useRunGraphDomain'
   import { RunGraphConfig } from '@/models/RunGraph'
   import { start, stop } from '@/objects'
-  import { WorkerMessage, worker } from '@/workers/runGraph'
+  // import { WorkerMessage, worker } from '@/workers/runGraph'
 
   const props = defineProps<{
     config: RunGraphConfig,
@@ -15,20 +14,18 @@
 
   const stage = ref<HTMLDivElement>()
 
-  worker.onmessage = onMessage
+  // worker.onmessage = onMessage
 
-  function onMessage({ data }: MessageEvent<WorkerMessage>): void {
-    switch (data.type) {
-      case 'pong':
-        console.log('pong')
-        return
-      default:
-        const exhaustive: never = data.type
-        throw new Error(`data.type does not have a handler associated with it: ${exhaustive}`)
-    }
-  }
-
-  useRunGraphDomain(() => props.config.runId, props.config.fetch)
+  // function onMessage({ data }: MessageEvent<WorkerMessage>): void {
+  //   switch (data.type) {
+  //     case 'pong':
+  //       console.log('pong')
+  //       return
+  //     default:
+  //       const exhaustive: never = data.type
+  //       throw new Error(`data.type does not have a handler associated with it: ${exhaustive}`)
+  //   }
+  // }
 
   onMounted(() => {
     if (!stage.value) {
