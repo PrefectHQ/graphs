e0a2b8b1673f6fda69ca0639f60ddd6d6a488da4
Craig Harshbarger <pleek91@gmail.com>
2023-10-22 16:44:41 -0500

    Add generic shape factories

---
+++ src/factories/bar.ts
@@ -0,0 +1,83 @@
+import { ColorSource, Container } from 'pixi.js'
+import { capFactory } from '@/factories/cap'
+import { rectangleFactory } from '@/factories/rectangle'
+
+type BarStyle = {
+  width: number,
+  height: number,
+  background: ColorSource,
+  radius: number,
+  capLeft?: boolean,
+  capRight?: boolean,
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function barFactory() {
+  const bar = new Container()
+  const rectangle = await rectangleFactory()
+  const { left, right, render: renderCaps } = await capFactory()
+
+  bar.addChild(rectangle)
+  bar.addChild(left)
+  bar.addChild(right)
+
+  async function render(style: BarStyle): Promise<Container> {
+    const { width, x, visible } = getRectangleStyles(style)
+
+    await renderCaps(style)
+
+    rectangle.visible = visible
+    rectangle.width = width
+    rectangle.height = style.height
+    rectangle.x = x
+
+    left.visible = getCapVisibility(style.capLeft, style.radius)
+    right.visible = getCapVisibility(style.capRight, style.radius)
+
+    right.x = style.radius + width
+
+    rectangle.tint = style.background
+    left.tint = style.background
+    right.tint = style.background
+
+    return bar
+  }
+
+  function getCapVisibility(enabled: boolean | undefined, radius: number): boolean {
+    if (radius === 0) {
+      return false
+    }
+
+    return enabled ?? true
+  }
+
+  function getRectangleStyles(style: BarStyle): { width: number, x: number, visible: boolean } {
+    const left = getCapVisibility(style.capLeft, style.radius)
+    const right = getCapVisibility(style.capRight, style.radius)
+
+    let caps = 0
+
+    if (left) {
+      caps += style.radius
+    }
+
+    if (right) {
+      caps += style.radius
+    }
+
+    const width = Math.max(style.width - caps, 0)
+    const visible = width > 0
+    const x = left ? style.radius : 0
+
+    return {
+      width,
+      visible,
+      x,
+    }
+  }
+
+  return {
+    bar,
+    render,
+  }
+}
---
+++ src/factories/rectangle.ts
@@ -0,0 +1,22 @@
+import { useSubscription } from '@prefecthq/vue-compositions'
+import { Graphics, RenderTexture, Sprite } from 'pixi.js'
+import { waitForApplication } from '@/objects'
+
+async function rectangle(): Promise<RenderTexture> {
+  const application = await waitForApplication()
+
+  const rectangle = new Graphics()
+  rectangle.beginFill('#fff')
+  rectangle.drawRect(0, 0, 1, 1)
+  rectangle.endFill()
+
+  const texture = application.renderer.generateTexture(rectangle)
+
+  return texture
+}
+
+export async function rectangleFactory(): Promise<Sprite> {
+  const texture = (await useSubscription(rectangle).promise()).response
+
+  return new Sprite(texture)
+}
