61090358eff0e2b5af41dd07ab9a93e1b8b10373
Craig Harshbarger <pleek91@gmail.com>
2023-11-04 21:14:40 -0500

    Simplify the playhead

--- src/factories/playhead.ts
+++ src/factories/playhead.ts
@@ -1,4 +1,3 @@
-import { Container } from 'pixi.js'
 import { rectangleFactory } from '@/factories/rectangle'
 import { waitForApplication, waitForViewport } from '@/objects'
 import { waitForConfig } from '@/objects/config'
@@ -12,55 +11,46 @@ export async function playheadFactory() {
   const application = await waitForApplication()
   const viewport = await waitForViewport()
   const config = await waitForConfig()
-  let scale = await waitForScale()
-  let initialized = false
-
-  const element = new Container()
-
   const playhead = await rectangleFactory()
-  element.addChild(playhead)
+
+  let scale = await waitForScale()
 
   emitter.on('scaleUpdated', updated => scale = updated)
-
-  const ticker = (): void => {
-    render()
-    checkViewport()
-  }
 
   function render(): void {
     playhead.width = config.styles.playheadWidth
     playhead.height = application.stage.height
     playhead.tint = config.styles.playheadColor
-    element.position.x = scale(new Date()) * viewport.scale._x + viewport.worldTransform.tx
+    playhead.position.x = scale(new Date()) * viewport.scale._x + viewport.worldTransform.tx
 
-    if (!initialized) {
-      application.ticker.add(ticker)
-      initialized = true
-    }
+    checkViewport()
   }
 
   function checkViewport(): void {
-    const playheadStartedVisible = element.position.x > 0 && element.position.x < application.screen.width
-
-    if (
-      !viewport.moving
-      && playheadStartedVisible
-      && element.position.x > application.screen.width - autoViewportUpdatePadding
-    ) {
-      const originalLeft = scale.invert(viewport.left)
-
-      viewport.zoomPercent(-0.1, true)
-      viewport.left = scale(originalLeft)
+    if (viewport.moving) {
+      return
     }
-  }
 
-  function stopTicker(): void {
-    application.ticker.remove(ticker)
+    const isVisible = playhead.position.x > 0 && playhead.position.x < application.screen.width
+
+    if (!isVisible) {
+      return
+    }
+
+    const shouldUpdateViewport = playhead.position.x > application.screen.width - autoViewportUpdatePadding
+
+    if (!shouldUpdateViewport) {
+      return
+    }
+
+    const originalLeft = viewport.left
+
+    viewport.zoomPercent(-0.1, true)
+    viewport.left = originalLeft
   }
 
   return {
-    element,
+    element: playhead,
     render,
-    stopTicker,
   }
 }
--- src/objects/playhead.ts
+++ src/objects/playhead.ts
@@ -1,41 +1,26 @@
-import { Container } from 'pixi.js'
 import { playheadFactory } from '@/factories/playhead'
 import { waitForApplication } from '@/objects/application'
 import { emitter } from '@/objects/events'
 import { waitForRunData } from '@/objects/nodes'
 
-let playheadContainer: Container | null = null
-
 export async function startPlayhead(): Promise<void> {
   const application = await waitForApplication()
-  const runGraphData = await waitForRunData()
-  const { element, render, stopTicker } = await playheadFactory()
+  const data = await waitForRunData()
+  const { element: playhead, render } = await playheadFactory()
 
-  const renderIfNotStopped = (): void => {
-    if (playheadContainer) {
-      render()
-    }
+  if (!data.end_time) {
+    application.stage.addChild(playhead)
+    application.ticker.add(render)
   }
 
-  if (!runGraphData.end_time) {
-    playheadContainer = element
-    application.stage.addChild(element)
-    render()
-  }
-
-  emitter.on('runDataUpdated', (data) => {
-    if (data.end_time && playheadContainer) {
-      application.stage.removeChild(playheadContainer)
-      stopTicker()
-      playheadContainer = null
+  emitter.on('runDataUpdated', ({ end_time }) => {
+    if (end_time) {
+      application.ticker.remove(render)
+      application.stage.removeChild(playhead)
     }
   })
-
-  emitter.on('viewportDateRangeUpdated', () => renderIfNotStopped())
-  emitter.on('layoutSettingsUpdated', () => renderIfNotStopped())
-  emitter.on('configUpdated', () => renderIfNotStopped())
 }
 
 export function stopPlayhead(): void {
-  playheadContainer = null
+  // do nothing
 }
