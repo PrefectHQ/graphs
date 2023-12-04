97dba14ff20eee2191ecebe9c79a8264c87be604
Craig Harshbarger <pleek91@gmail.com>
2023-12-04 15:20:08 -0600

    Remove subscriptions from caching logic

--- src/objects/cache.ts
+++ src/objects/cache.ts
@@ -1,22 +1,28 @@
-import { Action, ActionArguments, UseSubscription, useSubscription } from '@prefecthq/vue-compositions'
+import { Action } from '@prefecthq/vue-compositions'
 
-let subscriptions: UseSubscription<() => unknown>[] = []
+// Using any here because there isn't much benefit in typing this cache store. The key determines what type of value is returned.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let caches = new Map<string, any>()
 
 export function startCache(): void {
   // do nothing
 }
 
 export function stopCache(): void {
-  subscriptions.forEach(subscription => subscription.unsubscribe())
-  subscriptions = []
+  caches = new Map()
 }
 
-export async function cache<T extends Action>(action: T, parameters: ActionArguments<T>): Promise<ReturnType<T>> {
-  const subscription = useSubscription(action, parameters)
 
-  subscriptions.push(subscription)
+export async function cache<T extends Action>(action: T, parameters: Parameters<T>): Promise<ReturnType<T>> {
+  const key = `${action.toString()}-${JSON.stringify(parameters)}`
 
-  await subscription.promise()
+  if (caches.has(key)) {
+    return caches.get(key)
+  }
 
-  return subscription.response!
+  const value = await action(...parameters)
+
+  caches.set(key, value)
+
+  return value
 }
