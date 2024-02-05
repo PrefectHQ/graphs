74c551c483cdd3b14044e6013b470ed3b310f60d
Brandon Reid <brandonreidak@gmail.com>
2024-02-05 14:47:36 -0400

    add waitForIconCull

--- src/factories/icon.ts
+++ src/factories/icon.ts
@@ -1,11 +1,11 @@
 import { Sprite } from 'pixi.js'
 import { IconName } from '@/models/icon'
-import { waitForLabelCull } from '@/objects/culling'
+import { waitForIconCull } from '@/objects/culling'
 import { getIconTexture } from '@/textures/icon'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function iconFactory() {
-  const cull = await waitForLabelCull()
+  const cull = await waitForIconCull()
   const element = new Sprite()
 
   cull.add(element)
