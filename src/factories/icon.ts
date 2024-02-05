73c940ec781754f9561dfd155fca69cc080fce93
Brandon Reid <brandonreidak@gmail.com>
2024-02-05 14:41:02 -0400

    cull icons just like labels

--- src/factories/icon.ts
+++ src/factories/icon.ts
@@ -1,10 +1,14 @@
 import { Sprite } from 'pixi.js'
 import { IconName } from '@/models/icon'
+import { waitForLabelCull } from '@/objects/culling'
 import { getIconTexture } from '@/textures/icon'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function iconFactory() {
+export async function iconFactory() {
+  const cull = await waitForLabelCull()
   const element = new Sprite()
+
+  cull.add(element)
 
   async function render(icon: IconName): Promise<Sprite> {
     const texture = await getIconTexture(icon)
