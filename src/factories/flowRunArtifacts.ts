121b4c449fc67ba24106c502e2ffac280cfeb3fa
Brandon Reid <brandonreidak@gmail.com>
2024-02-08 07:51:03 -0400

    get artifacts showing

---
+++ src/factories/flowRunArtifacts.ts
@@ -0,0 +1,61 @@
+import { Container } from 'pixi.js'
+import { flowRunArtifactFactory, FlowRunArtifactFactory } from '@/factories/flowRunArtifact'
+import { Artifact } from '@/models'
+import { BoundsContainer } from '@/models/boundsContainer'
+import { waitForApplication } from '@/objects'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function flowRunArtifactsFactory() {
+  const application = await waitForApplication()
+
+  let container: Container | null = null
+  let internalData: Artifact[] | null = null
+  const artifacts: Map<string, FlowRunArtifactFactory> = new Map()
+
+  async function render(newData?: Artifact[]): Promise<void> {
+    if (newData) {
+      internalData = newData
+    }
+
+    if (!internalData) {
+      return
+    }
+
+    if (!container) {
+      createContainer()
+    }
+
+    const promises: Promise<BoundsContainer>[] = []
+
+    for (const artifact of internalData) {
+      promises.push(createArtifact(artifact))
+    }
+
+    await Promise.all(promises)
+
+    // TODO: Check collisions – account for different layouts
+  }
+
+  function createContainer(): void {
+    container = new Container()
+    application.stage.addChild(container)
+  }
+
+  async function createArtifact(artifact: Artifact): Promise<BoundsContainer> {
+    if (artifacts.has(artifact.id)) {
+      return artifacts.get(artifact.id)!.render()
+    }
+
+    const factory = await flowRunArtifactFactory(artifact)
+
+    artifacts.set(artifact.id, factory)
+
+    container!.addChild(factory.element)
+
+    return factory.render()
+  }
+
+  return {
+    render,
+  }
+}
