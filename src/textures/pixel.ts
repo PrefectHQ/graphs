590ff4778bc794149c5cf1e44011b4f7172caea7
Brandon Reid <brandonreidak@gmail.com>
2024-02-09 13:41:34 -0400

    repeat pixel textures

--- src/textures/pixel.ts
+++ src/textures/pixel.ts
@@ -1,4 +1,4 @@
-import { Graphics, RenderTexture } from 'pixi.js'
+import { Graphics, RenderTexture, WRAP_MODES } from 'pixi.js'
 import { waitForApplication } from '@/objects/application'
 import { cache } from '@/objects/cache'
 
@@ -10,7 +10,9 @@ async function texture(): Promise<RenderTexture> {
   rectangle.drawRect(0, 0, 1, 1)
   rectangle.endFill()
 
-  const texture = application.renderer.generateTexture(rectangle)
+  const texture = application.renderer.generateTexture(rectangle, {
+    wrapMode: WRAP_MODES.REPEAT,
+  })
 
   return texture
 }
