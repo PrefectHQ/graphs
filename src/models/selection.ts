1cfd2cdf6839e84d2ac28788c88c7ebe5d215a80
Brandon Reid <brandonreidak@gmail.com>
2024-02-09 10:41:19 -0400

    add artifact selection

--- src/models/selection.ts
+++ src/models/selection.ts
@@ -1,6 +1,15 @@
-import { RunGraphNode } from '@/models/RunGraph'
+import { RunGraphNode, Artifact } from '@/models'
 
 export type NodeSelection = {
   id: string,
   kind: RunGraphNode['kind'],
 }
+
+export type ArtifactSelection = {
+  kind: 'artifact',
+  id: string,
+}
+
+export type GraphItemSelection = NodeSelection | ArtifactSelection
+
+export type SelectableItem = RunGraphNode | Artifact
