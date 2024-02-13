738edb41103b109d0e22833866710dd8cffe29fb
Brandon Reid <brandonreidak@gmail.com>
2024-02-13 10:42:48 -0400

    resolve lame disableCulling prop and fix clustor styles

--- src/factories/label.ts
+++ src/factories/label.ts
@@ -2,14 +2,18 @@ import { BitmapText } from 'pixi.js'
 import { waitForLabelCull } from '@/objects/culling'
 import { waitForFonts } from '@/objects/fonts'
 
+type NodeLabelFactoryOptions = {
+  cullAtZoomThreshold?: boolean,
+}
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function nodeLabelFactory(disableCulling?: boolean) {
+export async function nodeLabelFactory({ cullAtZoomThreshold = true }: NodeLabelFactoryOptions = {}) {
   const { inter } = await waitForFonts()
   const cull = await waitForLabelCull()
 
   const label = inter('')
 
-  if (!disableCulling) {
+  if (cullAtZoomThreshold) {
     cull.add(label)
   }
 
